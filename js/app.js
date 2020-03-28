fdc9a58ea4525e405e02b490a623cc67321bf2d3
Mariano <petotronco@gmail.com>
2020-03-28 16:25:02 -0300

    Validacion de campos de entrada, en cuanto a que tengan contenido, y que el email al menos tenga un @

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // ------------------------ VARIABLES ------------------------
 
 const email = document.getElementById('email');
-const asunto = document.getElementById('asunto ');
+const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const btnEnviar = document.getElementById('enviar');
 
@@ -12,6 +12,11 @@ eventListeners();
 function eventListeners(){
     // inicio de la aplicacion
     document.addEventListener('DOMContentLoaded', inicioApp);
+
+    // para validar que los campos tengan algo
+    email.addEventListener('blur', validarCampo);
+    asunto.addEventListener('blur', validarCampo);
+    mensaje.addEventListener('blur', validarCampo);
 };
 
 // ------------------------ FUNCIONES ------------------------
@@ -26,8 +31,58 @@ function inicioApp(){
 };
 
 
+// valida que los campos hayan sigo cargados
+function validarCampo(){
+    // console.log('dentro del imput')
+
+    // validamos que no este vacio y la longitud del texto
+    // this en este caso es el campo actual, con lo cual valida
+    // para los 3 campos
+    validarLongitud(this);
+
+    // validar que el email tenga "@"
+    if(this.type === 'email'){
+        validarEmail(this);
+    }
+
+    // selector con todos los campos que contengan un error
+    let errores = document.querySelectorAll('.error');
+
+    if(email.value !== '' && asunto.value !== '' && mensaje.value !== ''){
+        // cuando no hy formulario con error
+        if(errores.length === 0){
+            btnEnviar.disabled = false;
+        };
+    };
+     
+};
+
+// valida la longitud del campo
+function validarLongitud(campo){
+    
+    // el campo no esta vacio
+    if(campo.value.length > 0){
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    }
+    // el campo si esta vacio
+    else{
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    };
+};
 
 
+function validarEmail(campo){
+    const mensaje = campo.value;
+    if(mensaje.indexOf('@') !== -1){
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    }else{
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+    };
+};
 
 
 
